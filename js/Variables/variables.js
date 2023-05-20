1cddff3412a3b8affb19b0628597ac8e5af6fc01
martincho111986 <martincho111986@gmail.com>
2023-05-19 22:26:25 -0300

    se agrego ejemplos en codigo

--- js/Variables/variables.js
+++ js/Variables/variables.js
@@ -1,14 +1,45 @@
+function ejemploVar() {
+    var x = 5;
+    
+    if (true) {
+      var x = 10; // La variable "x" es redeclarada dentro del bloque, afectando su valor fuera del bloque también
+      console.log(x); // Imprime 10
+    }
+    
+    console.log(x); // Imprime 10 debido al alcance a nivel de función
+  }
+  
+  ejemploVar();
 
-// console.log(mensaje = "hola");
+  function ejemploLet() {
+    let x = 5;
+    
+    if (true) {
+      let x = 10; // La variable "x" se declara dentro del bloque, no afectando su valor fuera del bloque
+      console.log(x); // Imprime 10
+    }
+    
+    console.log(x); // Imprime 5 debido al alcance a nivel de bloque
+  }
+  
+  ejemploLet();
 
-// var mensaje = "Hola Mundo";
-let nuevoMensaje = "Bye Bye World"
 
-// console.log(mensaje)
-console.log(nuevoMensaje)
-
-let edad = 24;
-if(edad > 18) {
-    let mensaje = "Puedes tomar alcohol"
-    console.log(mensaje)
-}
+  function ejemploConst() {
+    const PI = 3.1416;
+    
+    console.log(PI); // Imprime 3.1416
+    
+    // PI = 3.14; // Esto dará un error, no se puede reasignar un valor a una variable constante
+    
+    const persona = {
+      nombre: "Juan",
+      edad: 30
+    };
+    
+    persona.edad = 31; // Las propiedades de un objeto constante pueden ser modificadas
+    
+    console.log(persona); // Imprime { nombre: "Juan", edad: 31 }
+  }
+  
+  ejemploConst();
