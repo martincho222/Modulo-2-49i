dcbcb185a78c9405a7bf3f0d322c199f7ff5737b
Gonzalo Hurtado <gonzalomilano@gmail.com>
2023-06-02 19:43:49 -0300

    termine la primera tarea de string

---
+++ js/TareasJS/tareaString1.js
@@ -0,0 +1,8 @@
+/*La Escuela Café de las 3am solicita un sistema, donde le permita a sus estudiantes presentarse de una manera más interactiva y eficiente, por eso deciden acudir a un programador para crear un sistema, en el cual se podrá ingresar el nombre, edad, actividad favorita y deporte favorito en una tarjeta de presentación predeterminada. */
+
+alert("Bienvenido a la Escuela de Café 3am☕");
+let nombre = prompt("Ingresa tu nombre");
+let edad = prompt("Ingresa tu edad (solo numero)");
+let actividadFav = prompt("Ingresa tu actividad favorita🎭🎸💻");
+let deporteFav = prompt("Ingresa tu deporte favorito🏈🏐🏀");
+console.log("HOLA! Me llamo " + nombre + ". " + "Tengo " + edad + " años de edad. " + actividadFav + " es la actividad que me representa" + " y mi deporte favorito es " + deporteFav + ".");
---
+++ js/TareasJS/tareaString2.js
@@ -0,0 +1,12 @@
+/*Tarea String 2:
+Solicitar al usuario que ingrese una cadena y mostrar en consola la cadena en minúsculas. */
+/*TODAVIA NO LA TERMINÉ*/
+
+
+alert("Vamos a crear una cadena con animales");
+let cadena = prompt("Ingresá un animal");
+let cadena2 = prompt("Ahora otro animal");
+let cadena3 = prompt("Por favor otro animal");
+let cadenaCompleta = [cadena, cadena2, cadena3];
+
+console.log(cadenaCompleta.toLowerCase());
