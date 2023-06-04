f207eed2c8a20059766b5c2c3970d6bc3f32cc2b
Sergio Homet <103963551+sergiohomet@users.noreply.github.com>
2023-06-04 05:02:55 -0300

    Terminada la tarea sobre Strings

---
+++ js/tarea/number.js
@@ -0,0 +1,35 @@
+// Tarea Number 1:
+// Usando métodos matemáticos, ingresar 3 números:
+// ● Indicar cual es el mayor.
+// ● Indicar cual es el menor.
+// ● Obtener la raíz cuadrada del segundo número.
+
+//  Tarea Number 2:
+// Solicitar al usuario que ingrese dos números y mostrar en consola su suma.
+
+//  Tarea Number 3:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es par o impar.
+
+//  Tarea Number 4:
+// Generar un número aleatorio entre 1 y 100 y mostrarlo en consola.
+
+//  Tarea Number 5:
+// Solicitar al usuario que ingrese un número y mostrar en consola su valor absoluto.
+
+//  Tarea Number 6:
+// Solicitar al usuario que ingrese un número y mostrar en consola su raíz cuadrada.
+
+//  Tarea Number 7:
+// Solicitar al usuario que ingrese un número y mostrar en consola su potencia al cuadrado.
+
+//  Tarea Number 8:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es positivo, negativo o cero.
+
+//  Tarea Number 9:
+// Solicitar al usuario que ingrese dos números y mostrar en consola el mayor de ellos.
+
+//  Tarea Number 10:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es entero o decimal.
+
+//  Tarea Number 11:
+// Generar un número aleatorio entre dos valores ingresados por el usuario y mostrarlo en consola.
---
+++ js/tarea/strings.js
@@ -0,0 +1,59 @@
+// Tarea String 1:
+// La Escuela Café de las 3am solicita un sistema, donde le permita a sus estudiantes presentarse de una manera más interactiva y eficiente, por eso deciden acudir a un programador para crear un sistema, en el cual se podrá ingresar el nombre, edad, actividad favorita y deporte favorito en una tarjeta de presentación predeterminada.
+
+// alert("Bievenido a nuestro sistema, a continuación le pediremos sus datos");
+
+// let nombre = prompt("Ingrese su Nombre");
+// let edad = prompt("Ingrese su Edad");
+// let actividadFav = prompt("Ingrese su Actividad Favorita");
+// let deporteFav = prompt("Ingrese su Deporte Favorito");
+
+// console.log(`Bienvenido a la Escuela Café de las 3am, sus datos son: Nombre ${nombre} | Edad: ${edad} | Actividad Favorita: ${actividadFav} | Deporte Favorito: ${deporteFav}`);
+
+// Tarea String 2:
+// Solicitar al usuario que ingrese una cadena y mostrar en consola la cadena en minúsculas.
+
+// let cadena = prompt("Ingrese una cadena de texto en Mayúsculas");
+// console.log(cadena.toLowerCase());
+
+// Tarea String 3:
+// Solicitar al usuario que ingrese una cadena y mostrar en consola la primera letra en mayúscula y las demás en minúsculas.
+
+// let cadena = prompt("Ingrese una cadena de texto");
+// let letra = cadena.charAt(0);
+// let texto = cadena.slice(1)
+// console.log(letra.toUpperCase() + texto);
+
+// Tarea String 4:
+// Solicitar al usuario que ingrese una cadena y mostrar en consola la cadena invertida.
+
+// let cadena = prompt("Ingrese una cadena de texto");
+// let array = cadena.split("");
+// console.log(array.reverse());
+
+// Tarea String 5:
+// Solicitar al usuario que ingrese una cadena y un número, y mostrar en consola la cadena truncada en la posición del número ingresado.
+
+// let cadena = prompt("Ingrese una cadena de texto");
+// let numero = parseInt(prompt("Ingrese un número"));
+
+// console.log(cadena.slice(0 ,numero));
+
+// Tarea String 6:
+// Solicitar al usuario que ingrese una cadena y mostrar en consola la cantidad de vocales que contiene.
+
+// let cadena = prompt("Ingrese una cadena de texto");
+// let numeroVocales = cadena.match(/[aáeéiíoóuú]/gi).length;
+
+// console.log(numeroVocales);
+
+// Tarea String 7:
+// Solicitar al usuario que ingrese una cadena y una palabra, y mostrar en consola la cantidad de veces que la palabra aparece en la cadena.
+
+// let cadena = prompt("Ingrese una cadena de texto");
+// let palabra = prompt("Ingrese una palabra");
+// let encontrada = cadena.search(palabra);
+
+// console.log(encontrada);
+
+// PD: De está ultima tengo mis dudas de si estara bien.
