5169776fc79c6af4bf4234d47e18af0529c90063
pablopaul01 <juanpablosalomon@gmail.com>
2023-06-05 21:50:12 -0300

    Tareas numbers y strings

---
+++ js/TAREAS/numbers.js
@@ -0,0 +1,137 @@
+/* 
+Tarea Number 1:
+Usando métodos matemáticos, ingresar 3 números:
+● Indicar cual es el mayor.
+● Indicar cual es el menor.
+● Obtener la raíz cuadrada del segundo número.
+*/
+/*
+const numero1 = prompt("Ingresa el primer número: ");
+const numero2 = prompt("Ingresa el segundo número: ");
+const numero3 = prompt("Ingresa el tercer número: ");
+
+console.log("El número mas grande ingresado es: ",Math.max(numero1, numero2, numero3));
+console.log("El número mas chico ingresado es: ",Math.min(numero1, numero2, numero3));
+
+console.log("La raíz cuadradada de ",numero2, " es ",Math.sqrt(numero2) );
+*/
+
+/* 
+Tarea Number 2:
+Solicitar al usuario que ingrese dos números y mostrar en consola su suma.
+*/
+
+/* 
+const numero1 = prompt("Ingresa el primer número: ")*1;
+const numero2 = prompt("Ingresa el segundo número: ")*1;
+
+console.log("La suma de ambos es igual a : ",numero1+numero2);
+*/
+
+/* 
+Tarea Number 3:
+Solicitar al usuario que ingrese un número y mostrar en consola si es par o impar.
+*/
+
+/*
+const numero = prompt("Ingrese un número: ")*1;
+
+if (numero%2===0){
+    console.log("El número ingresado es par");
+}
+else
+{
+    console.log("El número ingresado es impar");
+}
+*/
+
+
+/* 
+Tarea Number 4:
+Generar un número aleatorio entre 1 y 100 y mostrarlo en consola.
+*/
+
+//console.log("El número aleatorio generado es: ", Math.floor(Math.random()*100+1));
+
+/* 
+Tarea Number 5:
+Solicitar al usuario que ingrese un número y mostrar en consola su valor absoluto.
+*/
+
+//const numero = parseFloat(prompt("Ingrese un número: "));
+
+
+//console.log("El valor absoluto del número ingresado es: ",Math.abs(numero));
+
+/*
+Tarea Number 6:
+Solicitar al usuario que ingrese un número y mostrar en consola su raíz cuadrada.
+*/
+
+//const numero = prompt("Ingrese un número: ");
+//console.log("La raíz cuadradada de ",numero, " es ",Math.sqrt(numero) );
+
+/*
+Tarea Number 7:
+Solicitar al usuario que ingrese un número y mostrar en consola su potencia al cuadrado.
+*/
+//const numero = prompt("Ingrese un número: ");
+//console.log("La potencia al cuadrado de ",numero, " es ",Math.pow(numero,2) );
+
+/*
+Tarea Number 8:
+Solicitar al usuario que ingrese un número y mostrar en consola si es positivo, negativo o cero.
+*/
+/* 
+const numero =parseFloat(prompt("Ingrese un número: "));
+if (numero >= 0){
+    if(numero > 0)
+    {
+        console.log("El número es positivo");
+    }
+    else
+    {
+        console.log("El número es 0");
+    }
+}
+else{
+    console.log("El número es negativo");
+}
+*/
+
+/*
+Tarea Number 9:
+Solicitar al usuario que ingrese dos números y mostrar en consola el mayor de ellos.
+*/
+
+//const numero1 = prompt("Ingresa el primer número: ");
+//const numero2 = prompt("Ingresa el segundo número: ");
+
+//console.log("El número mayor es: ", Math.max(numero1,numero2));
+
+/*
+Tarea Number 10:
+Solicitar al usuario que ingrese un número y mostrar en consola si es entero o decimal.
+*/
+/*
+const numero = parseFloat(prompt("Ingrese un número: "));
+
+if (Number.isInteger(numero)){
+    console.log("El número ingresado es un entero");
+}
+else
+{
+    console.log("El número ingresado es un decimal");
+}
+*/
+
+/*
+Tarea Number 11:
+Generar un número aleatorio entre dos valores ingresados por el usuario y mostrarlo en consola.
+*/
+/*
+const numero1 = parseFloat(prompt("Ingrese el primer valor:"));
+const numero2 = parseFloat(prompt("Ingrese el segundo valor:"));
+
+console.log("El valor aleatorio generado es ", Math.floor(Math.random()*(Math.max(numero1,numero2)-Math.min(numero1,numero2))+1));
+*/
---
+++ js/TAREAS/string.js
@@ -0,0 +1,80 @@
+/* Tarea String 1:
+La Escuela Café de las 3am solicita un sistema, donde le permita a sus estudiantes presentarse de una manera más interactiva y eficiente, por eso deciden acudir a un programador para crear un sistema, en el cual se podrá ingresar el nombre, edad, actividad favorita y deporte favorito en una tarjeta de presentación predeterminada.*/
+
+const nombre = prompt("Ingrese su nombre: ");
+const edad = prompt("Ingrese su edad: ");
+const actividadFavorita = prompt("Ingrese su actividad favorita: ");
+const deporteFavorito = prompt("Ingrese su deporte favorito: ");
+
+console.log("Alumno: ", nombre);
+console.log("Edad: ", edad);
+console.log("Actividad favorita: ", actividadFavorita);
+console.log("Deporte favorito: ", deporteFavorito);
+
+
+/* Tarea String 2:
+Solicitar al usuario que ingrese una cadena y mostrar en consola la cadena en minúsculas.*/
+
+//const cadena = prompt('Ingrese una cadena: ');
+//console.log(cadena.toLowerCase());
+
+/* Tarea String 3:
+Solicitar al usuario que ingrese una cadena y mostrar en consola la primera letra en mayúscula y las demás en minúsculas.*/
+
+//const cadena = prompt('Ingrese una cadena: ');
+//console.log(cadena.charAt(0).toUpperCase() + cadena.slice(1));
+
+/* Tarea String 4:
+Solicitar al usuario que ingrese una cadena y mostrar en consola la cadena invertida.*/
+//const cadena = prompt('Ingrese una cadena: ');
+//console.log(cadena.split("").reverse().join(""));
+
+/* Tarea String 5:
+Solicitar al usuario que ingrese una cadena y un número, y mostrar en consola la cadena truncada en la posición del número ingresado.*/
+
+/*const cadena = prompt('Ingrese una cadena de texto: ');
+let numero = prompt('Ingrese un número: ');
+if (numero > cadena.length){
+    console.log("El número ingresado es mayor a la longitud de la cadena");
+}
+    console.log(cadena.slice(numero));*/
+
+/* Tarea String 6:
+Solicitar al usuario que ingrese una cadena y mostrar en consola la cantidad de vocales que contiene.*/
+/*
+let cadena = prompt ("Ingrese una cadena de texto:");
+let contadorVocales=0;
+cadena = cadena.toLowerCase();
+for (let i=0; i < cadena.length; i++){
+    if (cadena.charAt(i)=='a' || cadena.charAt(i)=='e' || cadena.charAt(i)=='i' || cadena.charAt(i)=='o' || cadena.charAt(i)=='u' ){
+        ++contadorVocales;
+    }
+}
+
+console.log('La cadena posee ', contadorVocales, ' vocales'); 
+*/
+
+/* Tarea String 7:
+Solicitar al usuario que ingrese una cadena y una palabra, y mostrar en consola la cantidad de veces que la palabra aparece en la cadena.*/
+/*
+const cadena = prompt ("Ingrese una cadena de texto:");
+const cadenaBuscar= prompt("Ingrese una palabra:");
+
+let arregloCadena = cadena.split(" ");
+let contadorPalabra = 0;
+
+for (let i=0; i < arregloCadena.length; i++){
+    if (arregloCadena[i]===cadenaBuscar){
+        ++contadorPalabra;
+    }
+}
+
+if (contadorPalabra===0){
+    console.log("La palabra ingresa no se encuentra dentro de la cadena");
+}
+else
+{
+    console.log("La palabra se encuentra ",contadorPalabra, " veces dentro de la cadena");
+}
+*/
+
