c9a16204b8a79aabc454a080212aeb03cd8ea848
pablopaul01 <juanpablosalomon@gmail.com>
2023-06-08 15:06:01 -0300

    agrego tareas

---
+++ js/TAREAS/bucles.js
@@ -0,0 +1,26 @@
+/*
+    Nivel de dificultad: Fácil
+        1. Ejercicio: Imprimir los números del 1 al 10.
+*/
+for (let i =1; i <=10 ; i++){
+     console.log(i);
+   
+}
+    //     2. Ejercicio: Imprimir los números pares del 1 al 20.
+    //     3. Ejercicio: Imprimir los números impares del 1 al 15.
+    //     4. Ejercicio: Calcular la suma de los primeros 50 números enteros positivos.
+    // Nivel de dificultad: Moderado
+    //     5. Ejercicio: Imprimir la tabla de multiplicar del 5 (del 5 al 50).
+    //     6. Ejercicio: Imprimir los caracteres de un string en orden inverso.
+    //     7. Ejercicio: Calcular el factorial de un número dado (por ejemplo, 5! = 5 x 4 x 3 x 2 x 1).
+    // Nivel de dificultad: Difícil
+    //     8. Ejercicio: Imprimir los números primos del 1 al 50.
+    //     9. Ejercicio: Imprimir los elementos de un array de números.
+    //     10. Ejercicio: Calcular la suma de los dígitos de un número entero positivo.
+    //     11. Ejercicio: Imprimir una serie numérica especial: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, ...
+    // Nivel de dificultad: Experto
+    //     12. Ejercicio: Calcular el número de dígitos de un número entero.
+    //     13. Ejercicio: Imprimir una serie numérica especial: 1, -2, 3, -4, 5, -6, 7, -8, 9, -10, ...
+    //     14. Ejercicio: Calcular la suma de los dígitos pares de un número entero positivo.
+
+
