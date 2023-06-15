e4346824b5ce39a80c795a38bf2ef9cb1a63ab66
Gonzalo Hurtado <gonzalomilano@gmail.com>
2023-06-15 12:48:04 -0300

    termine todos los ejercicios de If Else

--- js/tareaEstructurasdeControl/tareaIfEls.js
+++ js/tareaEstructurasdeControl/tareaIfEls.js
@@ -11,8 +11,8 @@
 
 // Nivel de dificultad: Difícil
 // 6- Ejercicio if-else: Ordenar tres números de forma ascendente. LISTO
-// 7- Ejercicio if-else: Calcular el descuento aplicable a una compra según el monto.
-// 8- Ejercicio if-else: Determinar el tipo de triángulo según sus lados.
+// 7- Ejercicio if-else: Calcular el descuento aplicable a una compra según el monto. LISTO
+// 8- Ejercicio if-else: Determinar el tipo de triángulo según sus lados. LISTO
 
 
 // / ------RESUELTOS------
@@ -82,6 +82,7 @@
 
 
 // Nivel de dificultad: Difícil
+
 // 6- Ejercicio if-else: Ordenar tres números de forma ascendente.
 
 // alert("nivel DIFÍCIL");
@@ -108,4 +109,36 @@
 //     } else if (numero2 > numero1) {
 //         alert(`${numero3} ${numero2} ${numero1}`)
 //     }
+// }
+
+
+// 7- Ejercicio if-else: Calcular el descuento aplicable a una compra según el monto.
+
+// alert(`Las compras mayores a 15000 tienen descuento de 15off\nLas compras mayores a 30000 tienen descuento de 20off`);
+// let compraCliente = prompt(`Ingresa el producto que queres comprar`);
+
+// if (compraCliente >= 15000) {
+//     alert(`Su compra es de ${compraCliente}, tiene un descuento de 15off.\nEn total quedaría ${compraCliente - compraCliente * 0.15 }. Te estas ahorrando ${compraCliente * 0.15}`);
+// } else if (compraCliente >= 30000) {
+//     alert(`Su compra es de ${compraCliente}, tiene un descuento de 20off.\nEn total quedaría ${compraCliente - compraCliente * 0.20}. Te estas ahorrando ${compraCliente * 0.20}`);
+// } else if (compraCliente < 15000 && compraCliente > 0) {
+//     alert(`Su compra es de ${compraCliente}.\nTe faltan ${15000 - compraCliente} para tener 15off`);
+// } else if (compraCliente <= 0) {
+//     alert(`ingreso invalido`);
+// } else { alert(`ingreso invalido`); }
+
+
+// 8- Ejercicio if-else: Determinar el tipo de triángulo según sus lados.
+
+// alert(`voy a decirte que tipo de triangulo es con las medidas de los lados`);
+// let lado1 = prompt(`ingresa el largo del primer lado`);
+// let lado2 = prompt(`ingresa el largo del segundo lado`);
+// let lado3 = prompt(`ingresa el largo del tercer lado`);
+
+// if (lado1 === lado2 && lado2 === lado3) {
+//     alert("Triángulo equilátero");
+// } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+//     alert("Triángulo isósceles");
+// } else {
+//     alert("Triángulo escaleno");
 // }
