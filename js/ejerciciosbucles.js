9534fc09d93397120122dad0a4cac77ceb347e95
franv <fran.vicente03@gmail.com>
2023-06-16 19:19:53 -0300

    add exercises loops

--- js/ejerciciosbucles.js
+++ js/ejerciciosbucles.js
@@ -75,13 +75,125 @@ for (let i = texto.length - 1; i >= 0; i--) {
 
 // ejercicio 7
 
-let numero = parseInt(prompt("Ingrese un numero (max. 10)"));
+let numero = parseInt(prompt("Ingrese un numero (max. 14)"));
 let factorial = 1;
 
-for (let i = 1; i < numero + 1; i++) {
-  factorial *= i;
+if (numero > 0 && numero <= 14) {
+  for (let i = 1; i < numero + 1; i++) {
+    factorial *= i;
+  }
+} else {
+  console.log("No es posible calcular el factorial de dicho numero");
 }
 
 console.log(factorial);
 
 // ejercicio 8
+
+for (let numero = 2; numero <= 50; numero++) {
+  let esPrimo = true;
+
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      esPrimo = false;
+      break;
+    }
+  }
+
+  if (esPrimo) {
+    console.log(numero + " es primo");
+  }
+}
+
+// ejercicio 9
+
+let arrayNumerico = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arrayNumerico.length; i++) {
+  console.log(`Elemento [${i}] = ${arrayNumerico[i]}`);
+}
+
+// ejercicio 10
+
+numero = parseInt(prompt("Ingrese un numero entero positivo"));
+let sumador = 0;
+let aux = numero;
+let ultDig = 0;
+
+if (numero > 0) {
+  while (aux > 0) {
+    ultDig = aux % 10;
+    sumador += ultDig;
+    aux = parseInt(aux / 10);
+  }
+  console.log(`La suma de los digitos de ${numero} es ${sumador}`);
+} else {
+  console.log(`El numero ingresado (${numero}) es incorrecto`);
+}
+
+// ejercicio 11
+
+numero = parseInt(prompt("Ingrese la cantidad de numeros especiales (max. 20)"));
+
+if (numero < 21) {
+  for (let i = 1; i <= numero; i++) {
+    for (let j = 0; j < i; j++) {
+      console.log(i);
+    }
+  }
+} else {
+  console.log(`El numero ingresado (${numero}) es incorrecto`);
+}
+
+// ejercicio 12
+
+numero = parseInt(prompt("Ingrese un numero entero positivo"));
+sumador = 0;
+aux = numero;
+ultDig = 0;
+
+if (numero > 0) {
+  while (aux > 0) {
+    ultDig = aux % 10;
+    sumador++;
+    aux = parseInt(aux / 10);
+  }
+  console.log(`El numero de digitos de ${numero} es ${sumador}`);
+} else {
+  console.log(`El numero ingresado (${numero}) es incorrecto`);
+}
+
+// ejercicio 13
+
+numero = parseInt(prompt("Ingrese la cantidad de digitos que tendrá su serie numérica"));
+let arregloNum = [];
+
+for (let i = 0; i < numero; i++) {
+  if (i % 2 === 0) {
+    arregloNum[i] = (i + 1) * 1;
+  } else {
+    arregloNum[i] = (i + 1) * -1;
+  }
+}
+
+console.log(`La serie numerica especial es ${arregloNum}`);
+
+// ejercicio 14
+
+numero = parseInt(prompt("Ingrese un numero entero positivo"));
+sumador = 0;
+aux = numero;
+ultDig = 0;
+
+if (numero > 0) {
+  while (aux > 0) {
+    ultDig = aux % 10;
+    if (ultDig % 2 === 0) {
+      sumador += ultDig;
+    }
+    aux = parseInt(aux / 10);
+  }
+  console.log(`La suma de los digitos pares de ${numero} es ${sumador}`);
+} else {
+  console.log(`El numero ingresado (${numero}) es incorrecto`);
+}
