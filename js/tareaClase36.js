28f44ca4f8bc5a6503cb33755679408c175f9b81
Gonzalo Hurtado <gonzalomilano@gmail.com>
2023-06-29 11:32:18 -0300

    hice hasta el 13

--- js/tareaClase36.js
+++ js/tareaClase36.js
@@ -18,19 +18,19 @@
 
 // 7- Crea una clase Tiempo que represente una hora y minutos, y proporciona métodos para sumar y restar minutos a un objeto Tiempo. LISTO
 
-// 8- Implementa una clase Estudiante con propiedades como nombre, edad y una lista de notas, y un método para calcular el promedio de las notas.
+// 8- Implementa una clase Estudiante con propiedades como nombre, edad y una lista de notas, y un método para calcular el promedio de las notas. LISTO
 
-// 9- Crea una clase Circulo con una propiedad de radio y métodos para calcular el área y la circunferencia del círculo.
+// 9- Crea una clase Circulo con una propiedad de radio y métodos para calcular el área y la circunferencia del círculo. LISTO
 
-// 10- Extiende la clase Vehiculo del ejercicio anterior para incluir un método que verifique si el vehículo es nuevo (es decir, si su año de fabricación es mayor a un valor específico).
+// 10- Extiende la clase Vehiculo del ejercicio anterior para incluir un método que verifique si el vehículo es nuevo (es decir, si su año de fabricación es mayor a un valor específico). LISTO
 
 //  Nivel avanzado:
 
-// 11- Implementa una clase ListaTareas que permita agregar, eliminar y mostrar tareas. Cada tarea debe tener un título y una descripción.
+// 11- Implementa una clase ListaTareas que permita agregar, eliminar y mostrar tareas. Cada tarea debe tener un título y una descripción. LISTO
 
-// 12- Crea una clase Persona con propiedades como nombre, edad y género, y un método para presentarse (por ejemplo, "Hola, soy Juan, tengo 25 años y soy hombre").
+// 12- Crea una clase Persona con propiedades como nombre, edad y género, y un método para presentarse (por ejemplo, "Hola, soy Juan, tengo 25 años y soy hombre"). LISTO
 
-// 13- Extiende la clase Estudiante del ejercicio anterior para incluir un método que determine si el estudiante está aprobado (es decir, si su promedio de notas es mayor a un valor específico).
+// 13- Extiende la clase Estudiante del ejercicio anterior para incluir un método que determine si el estudiante está aprobado (es decir, si su promedio de notas es mayor a un valor específico). LISTO
 
 // 14- Implementa una clase CalculadoraCientifica que herede de la clase Calculadora y agregue métodos para calcular funciones matemáticas como seno, coseno y logaritmo.
 
@@ -211,7 +211,127 @@
 // ej1.sacarPromedio();
 
 
-
 // 9- Crea una clase Circulo con una propiedad de radio y métodos para calcular el área y la circunferencia del círculo.
 
+// class Circulo {
+//     constructor(radio) {
+//         this.radio = radio;
+//     }
+
+//     circunferencia() {
+//         let circunferencia = parseFloat((2 * Math.PI * this.radio).toFixed(3))
+//         console.log(circunferencia);
+//     }
+//     area() {
+//         let area = parseFloat(Math.pow((Math.PI * this.radio), 2).toFixed(3))
+//         console.log(area);
+//     }
+// }
+// ej1 = new Circulo(10);
+// ej1.circunferencia()
+// ej1.area()
+
 // 10- Extiende la clase Vehiculo del ejercicio anterior para incluir un método que verifique si el vehículo es nuevo (es decir, si su año de fabricación es mayor a un valor específico).
+
+// class Vehiculo {
+//     constructor(marca, modelo, año) {
+//         this.marca = marca;
+//         this.modelo = modelo;
+//         this.año = año;
+//     }
+//     descripcion() {
+//         console.log(`marca del auto:${this.marca} modelo:${this.modelo} año:${this.año}`);
+//     }
+// }
+
+// class Vehiculo2 extends Vehiculo {
+//     esNuevo() {
+//         if (this.año >= 2023) {
+//             console.log(`su auto es nuevo`);
+//         } else {
+//             console.log(`Su auto no es nuevo`);
+//         }
+//     }
+// }
+
+// let ej1 = new Vehiculo2("Fiat", "Uno", 1990);
+// ej1.esNuevo();
+// let ej2 = new Vehiculo2("Chevrolet", "Onix", 2023);
+// ej2.esNuevo();
+
+// 11- Implementa una clase ListaTareas que permita agregar, eliminar y mostrar tareas. Cada tarea debe tener un título y una descripción.
+
+// class ListaTareas {
+//     constructor(tarea, titulo, descripcion) {
+//         this.tarea = tarea;
+//         this.titulo = titulo;
+//         this.descripcion = descripcion;
+//     }
+//     agregar(titulo, descripcion) {
+//         this.tarea = [titulo, descripcion];
+//         console.log(`tarea agregada con exito`);
+//     }
+//     mostrar() {
+//         console.log(`${this.tarea}`);
+//     }
+//     eliminar() {
+//         this.tarea = null;
+//         console.log(`tarea eliminada con exito`);
+
+//     }
+// }
+
+// let ej1 = new ListaTareas()
+// ej1.agregar("pasea perro", "llevarlo a la plaza antes que se haga de noche");
+// ej1.mostrar();
+// ej1.eliminar();
+// ej1.mostrar();
+
+// 12- Crea una clase Persona con propiedades como nombre, edad y género, y un método para presentarse (por ejemplo, "Hola, soy Juan, tengo 25 años y soy hombre").
+
+// class Persona {
+//     constructor(nombre, edad, genero) {
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.genero = genero;
+//     }
+//     presentarse() {
+//         console.log(`Hola! Soy ${this.nombre}. tengo años ${this.edad} y soy ${this.genero}.`);
+//     }
+// }
+
+// let ej1 = new Persona("Goncal", 24, "Hombre");
+// ej1.presentarse();
+
+
+// 13- Extiende la clase Estudiante del ejercicio anterior para incluir un método que determine si el estudiante está aprobado (es decir, si su promedio de notas es mayor a un valor específico).
+
+// class Estudiante {
+//     constructor(nombre, edad, notas) {
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.notas = notas;
+//         this.promedio = 0;
+//     }
+//     sacarPromedio() {
+//         let resultado = this.notas.reduce((a, e) => a + e);
+//         this.promedio = resultado / this.notas.length;
+//         console.log(`el promedio de ${this.nombre} es ${this.promedio}`);
+//     }
+// }
+
+// Estudiante.prototype.estaAprobado = function() {
+//     if (this.promedio >= 6) {
+//         console.log(`Esta aprobado`);
+//     } else {
+//         console.log(`Desaprobado ${this.promedio}`);
+//     }
+// }
+
+
+// let ej1 = new Estudiante("Goncal", 24, [10, 9, 8])
+// ej1.sacarPromedio();
+// ej1.estaAprobado();
+// let ej2 = new Estudiante("Manuel", 18, [5, 5, 2])
+// ej2.sacarPromedio();
+// ej2.estaAprobado();
