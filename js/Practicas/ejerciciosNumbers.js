0adf63a8b806be9cbd33d11e2a92efa3c77366d8
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-07-09 09:19:58 -0300

    agregando los nuevos ejercicios de numbers

--- js/Practicas/ejerciciosNumbers.js
+++ js/Practicas/ejerciciosNumbers.js
@@ -1,37 +1,150 @@
-/*
-Tarea Number 1:
-Usando métodos matemáticos, ingresar 3 números:
-● Indicar cual es el mayor.
-● Indicar cual es el menor.
-● Obtener la raíz cuadrada del segundo número.
 
- Tarea Number 2:
-Solicitar al usuario que ingrese dos números y mostrar en consola su suma.
+// Tarea Number 1:
+// Usando métodos matemáticos, ingresar 3 números:
+// ● Indicar cual es el mayor.
+// ● Indicar cual es el menor.
+// ● Obtener la raíz cuadrada del segundo número.
 
- Tarea Number 3:
-Solicitar al usuario que ingrese un número y mostrar en consola si es par o impar.
+    // let numeroUno = 100;
+    // let numeroDos = 50;
+    // let numeroTres = 300;
 
- Tarea Number 4:
-Generar un número aleatorio entre 1 y 100 y mostrarlo en consola.
+    // let mayor = 0;
+    // let menor = 0;
+    // let medio = 0;
 
- Tarea Number 5:
-Solicitar al usuario que ingrese un número y mostrar en consola su valor absoluto.
+    // if(numeroUno>numeroDos){
+    //     if(numeroUno>numeroTres){
+    //         if(numeroDos>numeroTres){
+    //             mayor = numeroUno
+    //             medio = numeroDos
+    //             menor = numeroTres
 
- Tarea Number 6:
-Solicitar al usuario que ingrese un número y mostrar en consola su raíz cuadrada.
+    //         }else{
+    //             mayor = numeroUno
+    //             medio = numeroTres
+    //             menor = numeroDos
+    //         }
+    //     }else{
+    //         mayor = numeroTres
+    //         medio = numeroUno
+    //         menor = numeroDos
+    //     }
+    // }else if(numeroDos>numeroTres){
+    //     if(numeroTres>numeroUno){
+    //         mayor = numeroDos
+    //         medio = numeroTres
+    //         menor = numeroUno
 
- Tarea Number 7:
-Solicitar al usuario que ingrese un número y mostrar en consola su potencia al cuadrado.
+    //     }else{
+    //         mayor = numeroDos
+    //         medio = numeroUno
+    //         menor = numeroTres
+    //     }
+    // }else{
+    //     mayor = numeroTres
+    //     medio = numeroDos
+    //     menor = numeroUno
+    // }
+        
 
- Tarea Number 8:
-Solicitar al usuario que ingrese un número y mostrar en consola si es positivo, negativo o cero.
 
- Tarea Number 9:
-Solicitar al usuario que ingrese dos números y mostrar en consola el mayor de ellos.
+    // console.log(`el numero mayor es el ${mayor}, el menor es ${menor}, y la raiz cuadrada del otro numero es ${Math.sqrt(medio)}`)
 
- Tarea Number 10:
-Solicitar al usuario que ingrese un número y mostrar en consola si es entero o decimal.
+//  Tarea Number 2:
+// Solicitar al usuario que ingrese dos números y mostrar en consola su suma.
 
- Tarea Number 11:
-Generar un número aleatorio entre dos valores ingresados por el usuario y mostrarlo en consola.
-*/
+    // let numUno = parseInt(prompt("ingrese un numero"))
+    // let numDos = parseInt(prompt("ingrese otro numero"))
+
+    // console.log(`La sumade ${numUno} y ${numUno} es ${numUno+numDos}`);
+
+//  Tarea Number 3:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es par o impar.
+
+    // let num = parseInt(prompt("ingrese un numero para ver si es o no par"));
+
+    // if(num%2 === 0 ){
+    //     console.log(`${num} es par`)
+    // }else{
+    //     console.log(`${num} es impar`)
+    // }
+
+//  Tarea Number 4:
+// Generar un número aleatorio entre 1 y 100 y mostrarlo en consola.
+
+    // console.log(Math.round(Math.random()*100));
+
+//  Tarea Number 5:
+// Solicitar al usuario que ingrese un número y mostrar en consola su valor absoluto.
+
+    // let numAbs = parseInt(prompt("ingrese un numero para saber su valor absoluto"))
+
+    // console.log(`El valor absoluto de ${numAbs} es ${Math.abs(numAbs)}`)
+
+//  Tarea Number 6:
+// Solicitar al usuario que ingrese un número y mostrar en consola su raíz cuadrada.
+
+    // let num = parseInt(prompt("ingrese un numero para saber su raiz cuadrada"));
+
+    // console.log(`La raiz cuadrada de ${num} es ${Math.sqrt(num,2)}`)
+
+//  Tarea Number 7:
+// Solicitar al usuario que ingrese un número y mostrar en consola su potencia al cuadrado.
+
+    // let num = parseInt(prompt("ingrese un numero para saber su potencia cuadrada"));
+
+    // console.log(`La raiz cuadrada de ${num} es ${Math.pow(num,2)}`)
+
+//  Tarea Number 8:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es positivo, negativo o cero.
+
+    // let num = parseInt(prompt("Ingrese un numero para saber si es positivo, negativo o neutro"))
+
+    // if(num===0){
+    //     console.log(`${num} es igual a cero.`)
+    // }else if(num>0){
+    //     console.log(`${num} es positivo.`)
+    // }else{
+    //     console.log(`${num} es negativo.`)
+    // }
+
+//  Tarea Number 9:
+// Solicitar al usuario que ingrese dos números y mostrar en consola el mayor de ellos.
+
+    // let numUno = parseInt(prompt("Ingrese un numero"));
+    // let numDos = parseInt(prompt("Ingrese otro numero"));
+
+    // if(numUno>numUno){
+    //     console.log(`El mayor es ${numUno}`);
+    // }else{
+    //     console.log(`El mayor es ${numDos}`);
+    // }
+
+//  Tarea Number 10:
+// Solicitar al usuario que ingrese un número y mostrar en consola si es entero o decimal.
+
+
+    // let num = parseFloat(prompt("Ingrese un numero"));
+
+    // if(num === Math.floor(num)){
+    //     console.log(`${num} es un numero entero`)
+    // }else{
+    //     console.log(`${num} es decimal`)
+    // }
+
+//  Tarea Number 11:
+// Generar un número aleatorio entre dos valores ingresados por el usuario y mostrarlo en consola.
+
+    // let numUno = parseInt(prompt("ingrese un numero de inicio de rango para aleatorio"));
+    // let numDos = parseInt(prompt("ingrese un numero de fin de rango para aleatorio"));
+
+    // let min = numUno;
+    // let max = numDos
+
+    // if(numUno>numDos){
+    //     max = numUno
+    //     min = numDos
+    // }
+
+    // console.log(`${Math.round(Math.random()*(max-min)+min)}`)
