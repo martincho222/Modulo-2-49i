691b44b004142d8674c9048e9557666c6461bb4c
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-07-09 11:01:44 -0300

    Agregando los trabajos resueltos del if-else

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-
-// // console.log(mensaje = "hola");
-
-// // var mensaje = "Hola Mundo";
-// let nuevoMensaje = "Bye Bye World"
-
-// // console.log(mensaje)
-// console.log(nuevoMensaje)
-
-// let edad = 24;
-// if(edad > 18) {
-//     let mensaje = "Puedes tomar alcohol"
-//     console.log(mensaje)
-// }
-
 // Get the modal
 var modal = document.getElementById("myModalUno");
 
--- js/Practicas/if-else.js
+++ js/Practicas/if-else.js
@@ -1,19 +1,123 @@
-/*
-    Nivel de dificultad: Fácil
 
-        1. Ejercicio if-else: Determinar si un número es positivo, negativo o cero.
-        2. Ejercicio if-else: Determinar si un número es par o impar.
-        3. Ejercicio if-else: Verificar si un número es divisible por 3.
+    // // Nivel de dificultad: Fácil
 
-    Nivel de dificultad: Moderado
+    // //     1. Ejercicio if-else: Determinar si un número es positivo, negativo o cero.
 
-        4. Ejercicio if-else: Calcular el mayor de tres números.
-        5. Ejercicio if-else: Determinar si un año es bisiesto.
+    // let numberOne = 13;
 
-    Nivel de dificultad: Difícil
+    // if(numberOne===0){
+    //     console.log(`${numberOne} Es igual a cero.`)
+    // }else if(numberOne>0){
+    //     console.log(`${numberOne} Es positivo.`)
+    // }else{
+    //     console.log(`${numberOne} Es negativo.`)
+    // }
 
-        6. Ejercicio if-else: Ordenar tres números de forma ascendente.
-        7. Ejercicio if-else: Calcular el descuento aplicable a una compra según el monto.
-        8.  Ejercicio if-else: Determinar el tipo de triángulo según sus lados.
 
-*/
+    // //     2. Ejercicio if-else: Determinar si un número es par o impar.
+
+    // let numberTwo = 13;
+
+    // if(numberTwo%2 ===0){
+    //     console.log(`${numberTwo} Es par.`)
+    // }else{
+    //     console.log(`${numberTwo} Es impar.`)
+    // }
+
+    // //     3. Ejercicio if-else: Verificar si un número es divisible por 3.
+
+    // let numberThree = 13;
+
+    // if(numberThree%3 ===0){
+    //     console.log(`${numberThree} Es divisible en 3.`)
+    // }else{
+    //     console.log(`${numberThree} No es divisible en 3.`)
+    // }
+    // // Nivel de dificultad: Moderado
+
+    // //     4. Ejercicio if-else: Calcular el mayor de tres números.
+
+    // let numberFour1 = 10;
+    // let numberFour2 = 50;
+    // let numberFour3 = 100;
+
+    // if(numberFour1>=numberFour2){
+    //     if (numberFour1>=numberFour3){
+    //         console.log(numberFour1)
+    //     }
+    // }else if(numberFour2>numberFour3){
+    //     console.log(numberFour2)
+    // }else{
+    //     console.log(numberFour3)
+    // }
+    // //     5. Ejercicio if-else: Determinar si un año es bisiesto.
+    // let year = 2024;
+
+    // if(year%4===0){
+    //     console.log(`${year} es un año bisiesto.`)
+    // }else{
+    //     console.log(`${year} no es un año bisiesto.`)
+    // }
+
+    // Nivel de dificultad: Difícil
+
+    // //     6. Ejercicio if-else: Ordenar tres números de forma ascendente.
+
+    // let numUno = 10;
+    // let numDos = 1;
+    // let numTres = 100;
+
+    // if(numUno>=numDos){
+    //     if(numUno>numTres){
+    //         if(numDos>numTres){
+    //             console.log(`${numUno}, ${numDos}, ${numTres}`);
+    //         }else{
+    //             console.log(`${numUno}, ${numTres}, ${numDos}`);
+    //         }
+            
+    //     }else{
+    //         console.log(`${numTres}, ${numUno}, ${numDos}`)
+    //     }
+    // }else if(numDos>=numTres){
+    //     if (numTres>numUno){
+    //         console.log(`${numDos}, ${numTres}, ${numUno}`)
+    //     }else{
+    //         console.log(`${numDos}, ${numUno}, ${numTres}`)
+    //     }
+    // }else{
+    //     console.log(`${numTres}, ${numDos}, ${numUno}`)
+    // }
+
+
+
+    //     7. Ejercicio if-else: Calcular el descuento aplicable a una compra según el monto.
+
+    // let montoCompra = 1000;
+
+    // if(montoCompra<1000){
+        
+    // }else if (montoCompra<2000){
+    //     montoCompra = montoCompra*0.9 //10% de descuento para $1000
+    // }else{
+    //     montoCompra = montoCompra*0.8 // 20% de descuento $2000 o mas
+    // }
+
+    // console.log(montoCompra)
+
+    //     8.  Ejercicio if-else: Determinar el tipo de triángulo según sus lados.
+
+    // let ladoA = 2;
+    // let ladoB = 1;
+    // let ladoC = 2;
+
+    // console.log(ladoA!==ladoB);
+    // console.log(ladoB!==ladoC);
+
+
+    // if(ladoA===ladoB && ladoB === ladoC){
+    //     console.log(`Es un triangulo equilatero (3 lados iguales)`);
+    // }else if((ladoA!==ladoB) && (ladoB!== ladoC) && ladoA!==ladoC){
+    //     console.log(`Es un traingulo escaleno (3 lados distintos)`);
+    // }else{
+    //     console.log(`Es un triangulo isóceles (2 lados iguales)`);
+    // }
