8721c0ff9ad064d9b5f6dae06b6186bb435d39fc
martincho111986 <martincho111986@gmail.com>
2023-07-15 10:28:59 -0300

    se agrega ejercicio todo app

---
+++ js/Modules/app.js
@@ -0,0 +1,26 @@
+// import Usuarios from "./funciones.js";
+// console.log("object");
+
+//Exportación con nombre
+
+// export const nombre = "Conti";
+// export function suma(a,b) {
+//     return a + b;
+// };
+
+//Exportación por defecto
+// export default function multiplicar(a, b) {
+//     return a * b;
+// }
+
+//Importación con nombre
+// import algo from "../BOM/app";
+
+// algo()
+
+// Importación por defecto
+
+// import { multiplicar } from "../Practicas/paradigmas";
+// multiplicar(1,2)
+// const nuevoUsuario = new Usuarios("martin", "jerez", "martin@gmail.com", "1234556787")
+// console.log(nuevoUsuario);
---
+++ js/Modules/classes.js
@@ -0,0 +1,19 @@
+//  export class Usuarios {
+//     constructor(nombre, apellido, email, contrasena) {
+//         this.nombre = nombre
+//         this.apellido = apellido
+//         this.email = email
+//         this.contrasena = contrasena
+//     }
+// }
+
+// export class Producto {
+//     constructor() {
+
+//     }
+// }
+
+// export {
+//     Usuarios,
+//     Producto
+// }
